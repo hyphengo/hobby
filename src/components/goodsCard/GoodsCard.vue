ff99ab09fac9f7656390a752813c3f823a2a5773
zhaolii <782457949@qq.com>
2018-06-08 00:34:58 +0800

    购物车组件 first

---
+++ src/components/goodsCard/GoodsCard.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="goodscard">
+      <div class="goodscard-left">
+        <div class="goodscard-thumb">
+            <img :src="thumb" class="img" >
+        </div>
+        <div class="goodscard-content">
+            <div :class="title">
+                {{title}}
+            </div>
+            <div class="goodscard-price">
+                ￥{{price}}/{{unit}}
+            </div>
+        </div>
+      </div>
+      <div class="goodscard-right">
+        <vant-stepper v-model="num" />
+      </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+import { Stepper } from 'vant'
+
+@Component({
+  components: {
+    'VantStepper': Stepper
+  }
+})
+
+export default class GoodsCard extends Vue {
+  @Prop({ default: '' }) thumb: string
+  //   @Prop({ default: () => {} }) onAction: Function
+  @Prop({ default: null }) num: Number
+  @Prop({ default: null }) price: Number
+  @Prop({ default: '' }) title: string
+  @Prop({ default: '' }) unit: string
+
+  handleClick() {
+    // this.onAction()
+    // this.$emit('action')
+  }
+}
+</script>
+
+<style lang="scss">
+.goodscard {
+  border: 1px solid #ebeef5;
+  height: 146px;
+
+  &-left {
+  }
+
+  &-thumb {
+      width: 70px;
+      height: 78px;
+  }
+
+  &-right {
+    padding: 15px;
+  }
+
+  .boxer-action {
+    padding: 4px !important;
+  }
+}
+</style>
+
+
--- src/views/index/cart.vue
+++ src/views/index/cart.vue
@@ -1,23 +1,32 @@
 <template>
-  <div
-    class="cart"
-  >
-    <checkbox-group class="card-goods" v-model="checkedGoods">
+  <div class="cart">
+    <checkbox-group
+      class="card-goods"
+      v-model="checkedGoods"
+    >
       <vant-checkbox
         class="card-goods__item"
         v-for="item in goods"
         :key="item.id"
         :name="item.id"
       >
-        <vant-card
+        <!--<vant-card
           :title="item.title"
           :desc="item.desc"
           :num="item.num"
           :price="formatPrice(item.price)"
           :thumb="item.thumb"
+        />-->
+        <goods-card
+          :title="item.title"
+          :num="item.num"
+          :price="formatPrice(item.price)"
+          :thumb="item.thumb"
+          :unit="item.unit"
         />
       </vant-checkbox>
     </checkbox-group>
+
     <submit-bar
       :price="totalPrice"
       :button-text="submitBarText"
@@ -31,59 +40,60 @@
 import { Component, Vue } from 'vue-property-decorator'
 import { Checkbox, CheckboxGroup, Card, SubmitBar, Toast } from 'vant'
 // import { NavBar, Field, CellGroup, Button } from 'vant'
-
+import GoodsCard from '@/components/goodsCard/GoodsCard.vue'
 
 @Component({
   components: {
     'VantCheckbox': Checkbox,
     CheckboxGroup,
     'VantCard': Card,
-    // 'VantToast': Toast,
     SubmitBar,
+    GoodsCard
   }
 })
 export default class Cart extends Vue {
-  checkedGoods:any = ['1', '2', '3']
+  checkedGoods: any = ['1', '2', '3']
 
-      goods: any = [{
-        id: '1',
-        title: '进口香蕉',
-        desc: '约250g，2根',
-        price: 200,
-        num: 1,
-        thumb: 'https://img.yzcdn.cn/public_files/2017/10/24/2f9a36046449dafb8608e99990b3c205.jpeg'
-      }, {
-        id: '2',
-        title: '陕西蜜梨',
-        desc: '约600g',
-        price: 690,
-        num: 1,
-        thumb: 'https://img.yzcdn.cn/public_files/2017/10/24/f6aabd6ac5521195e01e8e89ee9fc63f.jpeg'
-      }, {
-        id: '3',
-        title: '美国伽力果',
-        desc: '约680g/3个',
-        price: 2680,
-        num: 1,
-        thumb: 'https://img.yzcdn.cn/public_files/2017/10/24/320454216bbe9e25c7651e1fa51b31fd.jpeg'
-      }]
+  goods: any = [{
+    id: '1',
+    title: '进口香蕉',
+    desc: '约250g，2根',
+    unit: '根',
+    price: 200,
+    num: 1,
+    thumb: 'https://img.yzcdn.cn/public_files/2017/10/24/2f9a36046449dafb8608e99990b3c205.jpeg'
+  }, {
+    id: '2',
+    title: '陕西蜜梨',
+    unit: '约600g',
+    price: 690,
+    num: 1,
+    thumb: 'https://img.yzcdn.cn/public_files/2017/10/24/f6aabd6ac5521195e01e8e89ee9fc63f.jpeg'
+  }, {
+    id: '3',
+    title: '美国伽力果',
+    unit: '3个',
+    price: 2680,
+    num: 1,
+    thumb: 'https://img.yzcdn.cn/public_files/2017/10/24/320454216bbe9e25c7651e1fa51b31fd.jpeg'
+  }]
 
-      get submitBarText() {
-        const count = this.checkedGoods.length
-        return '去买单' + (count ? `(${count})` : '')
-      }
+  get submitBarText() {
+    const count = this.checkedGoods.length
+    return '去买单' + (count ? `(${count})` : '')
+  }
 
-      get totalPrice() {
-        return this.goods.reduce((total, item) => total + (this.checkedGoods.indexOf(item.id) !== -1 ? item.price : 0), 0)
-      }
+  get totalPrice() {
+    return this.goods.reduce((total, item) => total + (this.checkedGoods.indexOf(item.id) !== -1 ? item.price : 0), 0)
+  }
 
-      formatPrice(price) {
-        return (price / 100).toFixed(2)
-      }
+  formatPrice(price) {
+    return (price / 100).toFixed(2)
+  }
 
-      onSubmit() {
-        Toast('点击结算')
-      }
+  onSubmit() {
+    Toast('点击结算')
+  }
 }
 
 </script>
