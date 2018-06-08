95b13c34b2084493b063c4d7cc42e0cf64c94562
李环冀 <lihuanji@lihuanji.(none)>
2018-06-09 00:26:46 +0800

    搜索部分逻辑

--- src/views/product/search.vue
+++ src/views/product/search.vue
@@ -2,31 +2,66 @@
   <div
     class="search"
   >
-  <van-search
-    v-model="value"
-    show-action
-    @search="onSearch"
-  >
-    <div slot="action" @click="onSearch">搜索</div>
-  </van-search>
+    <van-search
+      v-model="value"
+      show-action
+      @search="onSearch"
+      placeholder="搜索商品"
+    >
+      <div slot="action" @click="onSearch">
+        <van-button size="small">搜索</van-button>
+      </div>
+    </van-search>
+    <cell-group>
+      <van-cell
+        v-for="(item, index) in searchHistory"
+        :key="index"
+        :title="item"
+      >
+        <van-icon
+          slot="right-icon"
+          name="delete"
+          class="van-cell__right-icon"
+          @click="handleDelete(index)"
+        />
+      </van-cell>
+    </cell-group>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 // import { Getter, Action } from 'vuex-class'
-import { Search } from 'vant'
+import { Search, Cell, CellGroup } from 'vant'
+import ls from '@/util/localStorage'
+
+const lsSearchKey: string = 'COCOWA_SEARCH_HISTORY'
 
 @Component({
   components: {
-    'van-search': Search
+    'van-search': Search,
+    'van-cell': Cell,
+    CellGroup
   }
 })
-export default class Index extends Vue {
+export default class SearchPage extends Vue {
   value: string = ''
+  searchHistory: Array<string> = ls.get(lsSearchKey) || []
 
   onSearch() {
+    const indexKey = this.searchHistory.indexOf(this.value)
 
+    if (indexKey === -1) {
+      this.searchHistory.unshift(this.value)
+      ls.set(lsSearchKey, this.searchHistory)
+    } else {
+
+    }
+  }
+
+  handleDelete(index) {
+    this.searchHistory.splice(index, 1)
+    ls.set(lsSearchKey, this.searchHistory)
   }
 
   mounted() {
@@ -36,5 +71,8 @@ export default class Index extends Vue {
 </script>
 
 <style lang="scss">
-
+.van-search__action{
+  width: 150px;
+  text-align: center;
+}
 </style>
