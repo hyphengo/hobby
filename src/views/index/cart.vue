41cf7ca5850360b2e5dec87ad10ecffe12fb50e8
li zhao <lizhao@iMac-2.local>
2018-06-08 17:58:39 +0800

    commit

--- src/views/index/cart.vue
+++ src/views/index/cart.vue
@@ -1,43 +1,53 @@
 <template>
   <div class="cart">
-    <checkbox-group
-      class="card-goods"
-      v-model="checkedGoods"
-    >
-      <vant-checkbox
-        class="card-goods__item"
-        v-for="item in goods"
-        :key="item.id"
-        :name="item.id"
+    <div v-if="!hasgoods">
+      <div class="cart-list-title">
+        <vant-checkbox v-model="checkItemAll">干洗衣物</vant-checkbox>
+        <div class="font-24">满88免配送费，还差**元</div>
+      </div>
+      <checkbox-group
+        class="card-goods"
+        v-model="checkedGoods"
       >
-        <goods-card
-          :title="item.title"
-          :price="formatPrice(item.price)"
-          :thumb="item.thumb"
-          :unit="item.unit"
-        />
-        <div class="card-goods-step">
-          <vant-stepper v-model="item.num" />
-        </div>
-      </vant-checkbox>
-    </checkbox-group>
-    <!-- <div>
-      <span>全选</span>
-      <span>¥{{formatPrice(totalPrice)}}</span>
-      <span>去买单</span>
-    </div> -->
-    <submit-bar
-      :price="totalPrice"
-      :button-text="submitBarText"
-      :disabled="!checkedGoods.length"
-      @submit="onSubmit"
-    />
+        <vant-checkbox
+          class="card-goods__item"
+          v-for="item in goods"
+          :key="item.id"
+          :name="item.id"
+        >
+          <goods-card
+            :title="item.title"
+            :price="formatPrice(item.price)"
+            :thumb="item.thumb"
+            :unit="item.unit"
+          />
+          <div class="card-goods-step">
+            <vant-stepper v-model="item.num" />
+          </div>
+        </vant-checkbox>
+      </checkbox-group>
+      <div class="card-goods-btn">
+        <vant-checkbox v-model="checkedAll">全选</vant-checkbox>
+        <div class="totalprice">¥{{formatPrice(totalPrice)}}</div>
+        <div class="pay-btn" @click="onSubmit">去买单</div>
+      </div>
+    </div>
+    <div v-else class="card-nogoods">
+      <div class="tips">
+        <vant-icon name="shop" />
+        <p>没商品哦，快去搞点吧～</p>
+      </div>
+      <vant-button
+        text="去看看"
+        @click="goTosee"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import { Checkbox, CheckboxGroup, Card, SubmitBar, Toast, Stepper } from 'vant'
+import { Checkbox, CheckboxGroup, Card, SubmitBar, Toast, Stepper, Icon, Button } from 'vant'
 import GoodsCard from '@/components/goodsCard/GoodsCard.vue'
 
 @Component({
@@ -47,35 +57,41 @@ import GoodsCard from '@/components/goodsCard/GoodsCard.vue'
     'VantCard': Card,
     SubmitBar,
     GoodsCard,
-    'VantStepper': Stepper
+    'VantStepper': Stepper,
+    'VantIcon': Icon,
+    'VantButton': Button
   }
 })
 export default class Cart extends Vue {
   checkedGoods: any = ['1', '2', '3']
-
-  goods: any = [{
-    id: '1',
-    title: '进口香蕉',
-    desc: '约250g，2根',
-    unit: '根',
-    price: 200,
-    num: 1,
-    thumb: 'https://img.yzcdn.cn/public_files/2017/10/24/2f9a36046449dafb8608e99990b3c205.jpeg'
-  }, {
-    id: '2',
-    title: '陕西蜜梨',
-    unit: '约600g',
-    price: 690,
-    num: 3,
-    thumb: 'https://img.yzcdn.cn/public_files/2017/10/24/f6aabd6ac5521195e01e8e89ee9fc63f.jpeg'
-  }, {
-    id: '3',
-    title: '美国伽力果',
-    unit: '3个',
-    price: 2680,
-    num: 1,
-    thumb: 'https://img.yzcdn.cn/public_files/2017/10/24/320454216bbe9e25c7651e1fa51b31fd.jpeg'
-  }]
+  hasgoods: boolean = false
+  checkItemAll: boolean = false
+  checkedAll: boolean = false
+  goods: any = [
+    {
+      id: '1',
+      title: '进口香蕉',
+      desc: '约250g，2根',
+      unit: '根',
+      price: 200,
+      num: 1,
+      thumb: 'https://img.yzcdn.cn/public_files/2017/10/24/2f9a36046449dafb8608e99990b3c205.jpeg'
+    }, {
+      id: '2',
+      title: '陕西蜜梨',
+      unit: '约600g',
+      price: 690,
+      num: 3,
+      thumb: 'https://img.yzcdn.cn/public_files/2017/10/24/f6aabd6ac5521195e01e8e89ee9fc63f.jpeg'
+    }, {
+      id: '3',
+      title: '美国伽力果',
+      unit: '3个',
+      price: 2680,
+      num: 1,
+      thumb: 'https://img.yzcdn.cn/public_files/2017/10/24/320454216bbe9e25c7651e1fa51b31fd.jpeg'
+    }
+  ]
 
   get submitBarText() {
     const count = this.checkedGoods.length
@@ -91,16 +107,39 @@ export default class Cart extends Vue {
   }
 
   onSubmit() {
-    Toast('点击结算')
+    console.log('点击结算')
   }
+
+  goTosee() {
+    console.log('qukankan')
+  }
+
+
 }
 
 </script>
 
 <style lang="scss">
+.cart-list-title {
+  display: flex;
+  width: 100%;
+  height: 80px;
+  line-height: 80px;
+  justify-content: space-between;
+  padding: 0 22px;
+  font-size: 28px;
+  border-bottom: 1px solid #EDEDF0;
+  background: #fff;
+
+  .font-24 {
+    font-size: $--base-font;
+  }
+}
 .card-goods {
-  padding: 10px 0;
+  padding: 10px 20px;
   background-color: #fff;
+  margin-bottom: 15px;
+
   &__item {
     position: relative;
     background-color: #fff;
@@ -119,36 +158,86 @@ export default class Cart extends Vue {
     }
 
   }
-      &-step {
-        display: inline-block;
-        position: absolute;
-        right: 30px;
-        top: 50%;
-        margin-top: -22px;
+  &-step {
+    display: inline-block;
+    position: absolute;
+    right: 30px;
+    top: 50%;
+    margin-top: -22px;
 
-        .van-stepper__minus,
-        .van-stepper__plus {
-          border-radius: 50%;
-          width: 44px;
-          height: 44px;
-        }
+    .van-stepper__minus,
+    .van-stepper__plus {
+      border-radius: 50%;
+      width: 44px;
+      height: 44px;
+    }
 
-        .van-stepper__plus {
-          background: #3ACBCC;
-          border: none;
-        }
+    .van-stepper__plus {
+      background: #3ACBCC;
+      border: none;
+    }
 
-        .van-stepper__minus::after, .van-stepper__minus::before {
-          background: #3ACBCC;
-        }
+    .van-stepper__minus::after, .van-stepper__minus::before {
+      background: #3ACBCC;
+    }
 
-        .van-stepper__plus::after, .van-stepper__plus::before{
-          background: #fff;
-        }
+    .van-stepper__plus::after, .van-stepper__plus::before{
+      background: #fff;
+    }
 
-        .van-stepper__input {
-          border: none;
-        }
-      }
+    .van-stepper__input {
+      border: none;
+    }
+  }
+
+  &-btn {
+    width: 100%;
+    height: 98px;
+    line-height: 98px;
+    background: #fff;
+    position: absolute;
+    bottom: 98px;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 30px;
+    font-size: 28px;
+
+    .totalprice {
+      color: $--color-price;
+      font-size: 38px;
+    }
+    .pay-btn {
+      width: 232px;
+      background: $--color-base;
+      text-align: center;
+      color: #fff;
+    }
+  }
+}
+
+.card-nogoods {
+  text-align: center;
+  height: 100%;
+  padding-top: 50%;
+
+  .tips {
+    p {
+      font-size: 34px;
+      line-height: 4;
+      color: #666;
+    }
+    
+  }
+  .van-icon {
+    color: #666;
+  }
+  .van-button--normal,
+    .van-button--default {
+      width: 160px;
+      height: 80px;
+      line-height: 80px;
+      color: $--color-base;
+      border: 1px solid $--color-base;
+    }
 }
 </style>
