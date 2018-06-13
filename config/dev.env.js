8c153d133c137e58bc0afcc5a461d0bdaddb4022
lihuanji <158757774@qq.com>
2018-06-13 20:36:56 +0800

    1

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  CONTEXT: '"dev"', // 暂时的
+  CONTEXT: '"dev"',
   API_HOST: '"https://test-market.cocowa.com.cn/api/"',
   PUBLIC_PATH: '"/"'
 })
