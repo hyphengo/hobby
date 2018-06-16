fb6f210743fad8825eb73be4ac8e6d7f6574fe04
lihuanji <158757774@qq.com>
2018-06-16 22:01:57 +0800

    逛逛、购物车、订单详情页面添加“下拉刷新”操作（需求优化）

--- src/views/order/detail.vue
+++ src/views/order/detail.vue
@@ -1,144 +1,146 @@
 <template>
   <div class="detail">
-    <div class="detail-text" v-if="detail.state === 60">
-      <div class="detail-text-title">已完成</div>
-      <div v-if="detail.orderType === '1'">好服务值得再享受一次</div>
-      <div v-else>好东西值得再买一次</div>
-    </div>
-    <div class="detail-text" v-else-if="detail.state === 70">
-      <div class="detail-text-title">已关闭</div>
-      <div>订单已取消</div>
-    </div>
-    <div class="detail-status" v-else>
-      <van-steps :active="active">
-        <van-step
-          v-for="(data, index) in steplist"
-          :key="index"
-        >{{data.name}}</van-step>
-      </van-steps>
-      <div class="detail-tips" v-if="detail.orderType === '0'">
-        <span v-if="detail.state === 10 && leftTime > 0">未付款，{{leftTime}}分钟后将自动取消</span>
-        <span v-if="detail.state === 20">稍等，商家很快会接单的~</span>
-        <span v-if="detail.state === 30">稍等，商家很快会接单的~</span>
-        <span v-if="detail.state === 40">商家正在为您准备商品</span>
-        <span v-if="detail.state === 40 && detail.shippingMethod === '2'">随时可以去店铺拿货哦~</span>
-        <span v-if="detail.state === 50 && detail.shippingMethod === '1'">等待商品送上门来吧~</span>
+    <pull-refresh v-model="isLoading" @refresh="onRefresh">
+      <div class="detail-text" v-if="detail.state === 60">
+        <div class="detail-text-title">已完成</div>
+        <div v-if="detail.orderType === '1'">好服务值得再享受一次</div>
+        <div v-else>好东西值得再买一次</div>
       </div>
-      <div class="detail-tips" v-if="detail.orderType === '1'">
-        <span v-if=" detail.state === 10 && leftTime > 0">未付款，{{leftTime}}分钟后将自动取消</span>
-        <span v-if="detail.state === 30">稍等，商家很快会接单的~</span>
-        <span v-if="detail.state === 40 && detail.shippingMethod === '2'">约好了，把衣物送到店铺来吧~</span>
-        <span v-if="detail.state === 40 && detail.shippingMethod === '1'">准备好衣物，我们的人会准时找上门来的~</span>
-        <span v-if="detail.state === 110">我们很快会将衣物送至干洗中心~</span>
-        <span v-if="detail.state === 120">衣物已送至干洗中心，正在洗涤养护中~</span>
-        <span v-if="detail.state === 130">衣物洗好啦，辛苦您来店铺取一下哈~</span>
+      <div class="detail-text" v-else-if="detail.state === 70">
+        <div class="detail-text-title">已关闭</div>
+        <div>订单已取消</div>
       </div>
-      <div class="detail-tips" v-if="detail.orderType === '2'">
-        <span v-if=" detail.state === 10 && leftTime > 0">未付款，{{leftTime}}分钟后将自动取消</span>
-        <span v-if="detail.state === 30">美好事物值得等待，我蛙会按时出去采购回来的~</span>
-        <span v-if="detail.state === 130">商品已采购到店，随时可以去店铺拿货哦~</span>
+      <div class="detail-status" v-else>
+        <van-steps :active="active">
+          <van-step
+            v-for="(data, index) in steplist"
+            :key="index"
+          >{{data.name}}</van-step>
+        </van-steps>
+        <div class="detail-tips" v-if="detail.orderType === '0'">
+          <span v-if="detail.state === 10 && leftTime > 0">未付款，{{leftTime}}分钟后将自动取消</span>
+          <span v-if="detail.state === 20">稍等，商家很快会接单的~</span>
+          <span v-if="detail.state === 30">稍等，商家很快会接单的~</span>
+          <span v-if="detail.state === 40">商家正在为您准备商品</span>
+          <span v-if="detail.state === 40 && detail.shippingMethod === '2'">随时可以去店铺拿货哦~</span>
+          <span v-if="detail.state === 50 && detail.shippingMethod === '1'">等待商品送上门来吧~</span>
+        </div>
+        <div class="detail-tips" v-if="detail.orderType === '1'">
+          <span v-if=" detail.state === 10 && leftTime > 0">未付款，{{leftTime}}分钟后将自动取消</span>
+          <span v-if="detail.state === 30">稍等，商家很快会接单的~</span>
+          <span v-if="detail.state === 40 && detail.shippingMethod === '2'">约好了，把衣物送到店铺来吧~</span>
+          <span v-if="detail.state === 40 && detail.shippingMethod === '1'">准备好衣物，我们的人会准时找上门来的~</span>
+          <span v-if="detail.state === 110">我们很快会将衣物送至干洗中心~</span>
+          <span v-if="detail.state === 120">衣物已送至干洗中心，正在洗涤养护中~</span>
+          <span v-if="detail.state === 130">衣物洗好啦，辛苦您来店铺取一下哈~</span>
+        </div>
+        <div class="detail-tips" v-if="detail.orderType === '2'">
+          <span v-if=" detail.state === 10 && leftTime > 0">未付款，{{leftTime}}分钟后将自动取消</span>
+          <span v-if="detail.state === 30">美好事物值得等待，我蛙会按时出去采购回来的~</span>
+          <span v-if="detail.state === 130">商品已采购到店，随时可以去店铺拿货哦~</span>
+        </div>
       </div>
-    </div>
-    <div class="detail-item">
-      <address-card
-        v-if="detail.shippingMethod === '1'"
-        :info="detail"
-        :disabled="true"
-      />
-      <date-card
-        v-if="detail.shippingMethod === '1'"
-        :info="detail"
-        :disabled="true"
-      />
-      <invite-card
-        v-model="phone"
-        v-if="detail.shippingMethod === '2'"
-        :info="detail"
-        :orderType="detail.orderType"
-      />
-    </div>
-    <div class="detail-item">
-      <product-info
-        class="detail-product"
-        :order="detail"
-        :isDetail="true"
-      />
-    </div>
-    <div class="detail-item">
-      <ve-row class="productInfo-row van-hairline--bottom">
-        <ve-col :span="12">
-          实付金额
-        </ve-col>
-        <ve-col :span="12" textAlign="right" class="productInfo-grey price-color">
-          {{`￥${price(detail.total)}`}}
-        </ve-col>
-      </ve-row>
-      <ve-row class="productInfo-row van-hairline--bottom">
-        <ve-col :span="12">
-          支付方式
-        </ve-col>
-        <ve-col :span="12" textAlign="right" class="productInfo-grey">
-          {{payType}}
-        </ve-col>
-      </ve-row>
-    </div>
-    <div class="detail-item">
-      <ve-row class="productInfo-row van-hairline--bottom">
-        <ve-col :span="12">
-          下单时间
-        </ve-col>
-        <ve-col :span="12" textAlign="right" class="productInfo-grey">
-          {{createDate}}
-        </ve-col>
-      </ve-row>
-      <ve-row class="productInfo-row van-hairline--bottom">
-        <ve-col :span="12">
-          订单号
-        </ve-col>
-        <ve-col :span="12" textAlign="right" class="productInfo-grey">
-          {{detail.id}}
-        </ve-col>
-      </ve-row>
-    </div>
-    <div v-if="detail.state === 10" class="detail-btn">
-      <van-button
-        type="default"
-        class="btn"
-        @click="handleCancel"
-      >
-        取消
-      </van-button>
-      <van-button
-        type="default"
-        :class="['btn', 'active']"
-        @click="handlePay"
-      >
-        买单
-      </van-button>
-    </div>
-    <div v-else class="detail-btn">
-      <a
-        v-if="detail.state !== 10"
-        class="btn van-button van-button--default van-button--normal"
-        href="tel:‭02886210761‬"
-      >
-        联系店铺
-      </a>
-      <van-button
-        type="default"
-        :class="['btn', 'active']"
-        @click="handleAgain"
-        v-if="detail.state === 30 || detail.state === 60 || detail.state === 70"
-      >
-        再买
-      </van-button>
-    </div>
+      <div class="detail-item">
+        <address-card
+          v-if="detail.shippingMethod === '1'"
+          :info="detail"
+          :disabled="true"
+        />
+        <date-card
+          v-if="detail.shippingMethod === '1'"
+          :info="detail"
+          :disabled="true"
+        />
+        <invite-card
+          v-model="phone"
+          v-if="detail.shippingMethod === '2'"
+          :info="detail"
+          :orderType="detail.orderType"
+        />
+      </div>
+      <div class="detail-item">
+        <product-info
+          class="detail-product"
+          :order="detail"
+          :isDetail="true"
+        />
+      </div>
+      <div class="detail-item">
+        <ve-row class="productInfo-row van-hairline--bottom">
+          <ve-col :span="12">
+            实付金额
+          </ve-col>
+          <ve-col :span="12" textAlign="right" class="productInfo-grey price-color">
+            {{`￥${price(detail.total)}`}}
+          </ve-col>
+        </ve-row>
+        <ve-row class="productInfo-row van-hairline--bottom">
+          <ve-col :span="12">
+            支付方式
+          </ve-col>
+          <ve-col :span="12" textAlign="right" class="productInfo-grey">
+            {{payType}}
+          </ve-col>
+        </ve-row>
+      </div>
+      <div class="detail-item">
+        <ve-row class="productInfo-row van-hairline--bottom">
+          <ve-col :span="12">
+            下单时间
+          </ve-col>
+          <ve-col :span="12" textAlign="right" class="productInfo-grey">
+            {{createDate}}
+          </ve-col>
+        </ve-row>
+        <ve-row class="productInfo-row van-hairline--bottom">
+          <ve-col :span="12">
+            订单号
+          </ve-col>
+          <ve-col :span="12" textAlign="right" class="productInfo-grey">
+            {{detail.id}}
+          </ve-col>
+        </ve-row>
+      </div>
+      <div v-if="detail.state === 10" class="detail-btn">
+        <van-button
+          type="default"
+          class="btn"
+          @click="handleCancel"
+        >
+          取消
+        </van-button>
+        <van-button
+          type="default"
+          :class="['btn', 'active']"
+          @click="handlePay"
+        >
+          买单
+        </van-button>
+      </div>
+      <div v-else class="detail-btn">
+        <a
+          v-if="detail.state !== 10"
+          class="btn van-button van-button--default van-button--normal"
+          href="tel:‭02886210761‬"
+        >
+          联系店铺
+        </a>
+        <van-button
+          type="default"
+          :class="['btn', 'active']"
+          @click="handleAgain"
+          v-if="detail.state === 30 || detail.state === 60 || detail.state === 70"
+        >
+          再买
+        </van-button>
+      </div>
+    </pull-refresh>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Inject } from 'vue-property-decorator'
-import { SubmitBar, Step, Steps } from 'vant'
+import { SubmitBar, Step, Steps, PullRefresh } from 'vant'
 import ProductInfo from '@/components/productInfo/index.vue'
 import AddressCard from '@/components/address-card/index.vue'
 import DateCard from '@/components/date-card/index.vue'
@@ -156,7 +158,8 @@ import wxs from '@/wxsdk'
     DateCard,
     InviteCard,
     'VanStep': Step,
-    'VanSteps': Steps
+    'VanSteps': Steps,
+    PullRefresh
   }
 })
 export default class Confirm extends Vue {
@@ -167,10 +170,19 @@ export default class Confirm extends Vue {
   steplist: any = []
   leftTime: number
   payType: string = '微信支付'
+  isLoading: boolean = false
 
   @Inject('reload') reload: any
 
+  onRefresh() {
+    this.init()
+  }
+
   mounted() {
+    this.init()
+  }
+
+  init() {
     this.id = this.$route.params.id
 
     getOrderDetail(this.id).then(res => {
@@ -225,6 +237,8 @@ export default class Confirm extends Vue {
           this.active = index
         }
       })
+
+      this.isLoading = false
     })
   }
 
