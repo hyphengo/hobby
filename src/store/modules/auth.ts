763f6e4d609607ff1ae87b6442c73306b3fd71fb
lihuanji <158757774@qq.com>
2018-06-27 09:39:49 +0800

    add registry

--- src/store/modules/auth.ts
+++ src/store/modules/auth.ts
@@ -5,16 +5,19 @@ const ActionType = {
   SET_TOKEN: 'SET_TOKEN',
   CLEAR_USER: 'CLEAR_USER',
   IS_AUTH: 'IS_AUTH',
+  SET_REGISTRY: 'SET_REGISTRY'
 }
 
 type StateType = {
   token?: string | null;
   isAuth?: boolean;
+  registry?: boolean;
 }
 
 const state: StateType = {
   token: null,
   isAuth: false,
+  registry: false,
 }
 
 const mutations = {
@@ -25,9 +28,16 @@ const mutations = {
       state.token = token
     }
   },
+  // registry
+  [ActionType.SET_REGISTRY](state: StateType, registry: boolean) {
+    if (registry) {
+      state.registry = registry
+    }
+  },
   // Clear 所有数据
   [ActionType.CLEAR_USER](state: StateType) {
     state.token = null
+    state.registry = false
     ls.remove(TOKEN)
   },
   // 是否登录
@@ -39,7 +49,8 @@ const mutations = {
 const actions = {
   setUser({ commit, dispatch }, data: any) {
     commit(ActionType.IS_AUTH, true)
-    commit(ActionType.SET_TOKEN, data)
+    commit(ActionType.SET_TOKEN, data.token)
+    commit(ActionType.SET_REGISTRY, data.registry)
   },
   clearAuth({ commit }) {
     commit(ActionType.CLEAR_USER)
@@ -50,6 +61,7 @@ const actions = {
 const getters = {
   isAuth: (state: StateType) => state.isAuth,
   token: (state: StateType) => state.token,
+  registry: (state: StateType) => state.registry,
 }
 
 export default {
